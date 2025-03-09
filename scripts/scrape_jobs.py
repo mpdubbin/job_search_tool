3113c34c636e2ee714d6cd888c244476d64b33bc
mdubbin <mpdubbin@gmail.com>
2025-03-09 16:58:37 -0400

    function for linkedin login workflow; update scrape_jobs.py for linkedin login workflow

--- scripts/scrape_jobs.py
+++ scripts/scrape_jobs.py
@@ -1,32 +1,40 @@
+from playwright.sync_api import sync_playwright
+
+from dotenv import load_dotenv
+from module_scrape import *
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
+# Load environment variables
+load_dotenv()
+LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
+
+
+
+
 # Load job posting URLs from file
 with open("data/rag/job_urls.txt", "r") as f:
     job_urls = [line.strip() for line in f.readlines()]
 
-# Set up Selenium WebDriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
 # Create directory to store raw HTML
 os.makedirs("data/rag/raw_html", exist_ok=True)
 
-# Scrape each URL
+
+# Scrape n' Save each URL
 for idx, url in enumerate(job_urls):
-    driver.get(url)
-    time.sleep(10) 
-    
-    html_content = driver.page_source
-
-    # Save HTML file
-    with open(f"data/rag/raw_html/job_{idx}.html", "w", encoding="utf-8") as f:
-        f.write(html_content)
-
-    print(f"Saved: data/rag/raw_html/job_{url}.html")
-
-driver.quit()
+    if 'linkedin' in url:
+        html_content = get_full_page_url_linkedin(url, LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
+        with open(f"data/rag/raw_html/job_{idx}.html", "w", encoding="utf-8") as f:
+            f.write(html_content)
+        print(f"Saved: {url}")
+        time.sleep(10)
+    else:
+        html_content = get_full_page_html(url)
+        with open(f"data/rag/raw_html/job_{idx}.html", "w", encoding="utf-8") as f:
+            f.write(html_content)
+        print(f"Saved: {url}")
+        time.sleep(10) 
